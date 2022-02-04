4ca4ab9e75dac326fe5ad13326351ff4fc0408b2
Leo Crapart <leocrapart@yahoo.fr>
2022-02-04 17:06:17 +0100

    images dezoom

--- src/lib/ProductCard.svelte
+++ src/lib/ProductCard.svelte
@@ -5,11 +5,9 @@
   export let url = "/fraise";
 </script>
 
-<a href={url} class="w-52 mx-4 my-4">
+<a href={url} class="w-52 h-60 mx-4 my-4">
   <div class="flex flex-col ">
-    <div class="flex items-end h-52">
-      <img class=" rounded-t" src={img_url} alt={"confiture de " + name} />
-    </div>
+    <img class="rounded-t" src={img_url} alt={"confiture de " + name} />
 
     <div class="flex justify-between bg-gray-200 rounded-b px-4 py-3 ">
       <div>{name}</div>
